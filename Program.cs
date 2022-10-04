1eb3a0b8e0c64bbc5b131370f0515027f77316cd
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-03 21:08:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PlayingCards
+{
+    internal class Program
+    {
+        private static readonly Random random = new Random();
+        static void Main(string[] args)
+        {
+            int randomValue = random.Next(1,14);
+            int randomSuit = random.Next(4);
+            Card card = new Card((Values)randomValue, (Suits)randomSuit);
+            Console.WriteLine(card.Name);
+        }
+    }
+}
